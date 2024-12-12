cad74dba06298d117e722f755448c265a840cff8
swapnilkuthe <swapnilkuthe20@gmail.com>
2024-12-13 00:40:05 +0530

    create register and login api succeessfully

--- MEARN_Website/API/Controllers/user.js
+++ MEARN_Website/API/Controllers/user.js
@@ -1,4 +1,5 @@
 import { User } from "../Models/user.js";
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -6,14 +7,28 @@ export const register = async (req, res) => {
         let user = await User.findOne({ email })
         if (user) return res.json({ message: "User Already Exist", success: false });
 
-        user = await User.create({ name, email, password });
-        res.json({ message: "User created successfully !!", success: true })
+        const hasPass = await bcrypt.hash(password, 10);
+
+        user = await User.create({ name, email, password: hasPass });
+        res.json({ message: "User created successfully !!", success: true, user })
     } catch (error) {
         res.json({ message: error.message })
     }
 }
 
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) return res.json({ message: "Email not found", success: false })
 
+        const validPass = await bcrypt.compare(password, user.password)
+        if (!validPass) return res.json({ message: "Password is wrong", success: false })
 
+        res.json({ message: `Welcome user : ${User.name}`, success: true, user })
+    } catch (error) {
+        res.json({ message: error.message })
+    }
+}
 
 
--- MEARN_Website/API/Routers/user.js
+++ MEARN_Website/API/Routers/user.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { register } from '../Controllers/user.js';
+import { login, register } from '../Controllers/user.js';
 
 const router = express.Router();
 
 // registered user
 router.post('/register', register)
+
+// login user
+router.post('/login', login)
 
 export default router;
