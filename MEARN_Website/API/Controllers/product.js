bcf8d52b5e45f21635b9777e59820f78a01c0630
swapnilkuthe <swapnilkuthe20@gmail.com>
2024-12-18 18:51:38 +0530

    Create get specific product by id done

--- MEARN_Website/API/Controllers/product.js
+++ MEARN_Website/API/Controllers/product.js
@@ -1,7 +1,6 @@
 import { Products } from "../Models/product.js"
 
 // add product
-
 export const addProduct = async (req, res) => {
     const { title, description, price, category, qty, imgSrc, createdAt } = req.body;
     try {
@@ -16,12 +15,24 @@ export const addProduct = async (req, res) => {
     }
 }
 
+// get all products
 export const getProducts = async (req, res) => {
-
     try {
         let products = await Products.find().sort({ createdAt: -1 });
         res.json({ message: "All Products", products, success: true });
 
+    } catch (error) {
+        res.json(error.message)
+    }
+}
+
+// get specific product by id 
+export const getProductByID = async (req, res) => {
+    const id = req.params.id;
+    try {
+        let product = await Products.findById(id);
+        if (!product) return res.json({ message: "Invalid Id" })
+        res.json({ message: "specific product", product })
     } catch (error) {
         res.json(error.message)
     }
--- MEARN_Website/API/Routers/product.js
+++ MEARN_Website/API/Routers/product.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addProduct, getProducts } from "../Controllers/product.js";
+import { addProduct, getProductByID, getProducts } from "../Controllers/product.js";
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.post('/add', addProduct)
 
 // get products
 router.get('/allProducts', getProducts)
+
+// get specific product
+router.get('/:id', getProductByID)
 
 export default router;
