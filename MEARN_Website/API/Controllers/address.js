69fb7b9a2020fe63f17d6f588620fe5a8e92fab0
swapnilkuthe <swapnilkuthe20@gmail.com>
2024-12-19 15:34:18 +0530

    Get address api done

--- MEARN_Website/API/Controllers/address.js
+++ MEARN_Website/API/Controllers/address.js
@@ -1,5 +1,6 @@
 import { Address } from "../Models/Address.js";
 
+// add address controller
 export const addAddress = async (req, res) => {
     const { fullname, address, city, state, country, pinCode, phoneNumber } = req.body;
 
@@ -11,6 +12,12 @@ export const addAddress = async (req, res) => {
     } catch (error) {
         res.json(error.message)
     }
+}
+
+// get address controller
+export const getAddress = async (req, res) => {
+    let address = await Address.find({ userId: req.user }).sort({ createdAt: -1 });
+    res.json({ message: "address", userAddress: address[0] })
 }
 
 
--- MEARN_Website/API/Routers/address.js
+++ MEARN_Website/API/Routers/address.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { addAddress } from '../Controllers/address.js';
+import { addAddress, getAddress } from '../Controllers/address.js';
 import { Authenticated } from '../Middlewares/Auth.js';
 
 const router = express.Router();
 
-// add adress
+// add address
 router.post('/add', Authenticated, addAddress)
 
+// get address
+router.get('/get', Authenticated, getAddress)
 
 export default router;
